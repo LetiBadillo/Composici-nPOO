21e8d105782fad4c9712c7b9db085cf44adf310c
LetiBadillo <letibadillog@hotmail.com>
2016-04-06 17:45:13 +0300

    Update POORelaciondeComposicion.java

--- POORelaciondeComposicion.java
+++ POORelaciondeComposicion.java
@@ -17,7 +17,8 @@ public class POORelacióndeComposición {
     public static void main(String[] args) {
         // TODO code application logic here
      Telefono starTac = new Telefono();
-    
+     
+   
      starTac.EA.presionar();
     System.out.println("El starTac está "+starTac.EA.IO);
     starTac.EA.presionar();
@@ -25,5 +26,19 @@ public class POORelacióndeComposición {
     
     starTac.setTeclado();
     starTac.printTeclado();
+    
+    starTac.newContacto("Leti", "123456", "letibadillog@hotmail.com");
+    starTac.newContacto("Deni", "987654", "denisse123654@hotmail.com");
+    starTac.newContacto("Lili", "147258", "lili@hotmail.com");
+    starTac.newContacto("Arón", "248644", "denisse789@hotmail.com");
+    starTac.newContacto("Deni", "787552", "denissetas@hotmail.com");
+    System.out.println();
+    System.out.println("Lista de Contactos");
+    starTac.printAgenda();
+    System.out.println();
+    starTac.printIndice(5);
+    starTac.printIndice(2);
+    
+    
     }
-}
+    }
